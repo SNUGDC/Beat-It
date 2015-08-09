0b3e9afba15f1b00db898008cb2ac512eba71085
Kais_DkM <kais0903@gmail.com>
2015-08-09 16:25:42 +0900

    added handclap at each note

--- Assets/NoteMover.cs
+++ Assets/NoteMover.cs
@@ -7,19 +7,31 @@ public class NoteMover : MonoBehaviour {
 	public GameObject LeftObject;
 	public GameObject RightObject;
 
+	private bool isMoving;
+
 	void Start() {
+		isMoving = true;
 	}
 	
 	void Update() {
-		float speed = Camera.main.orthographicSize * 16 / 9
-					  / NoteMover.NoteDelay * Time.deltaTime;
-		LeftObject.transform.Translate(speed, 0, 0);
-		RightObject.transform.Translate(-speed, 0, 0);
-		// delete note from display
-		if(LeftObject.transform.position.x >= 0) {
-			NoteData.Kill();
-			Object.Destroy(this.gameObject);
-			transform.parent.GetComponent<BeatGenerator>().NoteList.Dequeue();
+		if(isMoving) {
+			float speed = Camera.main.orthographicSize * 16 / 9
+						  / NoteMover.NoteDelay * Time.deltaTime;
+			LeftObject.transform.Translate(speed, 0, 0);
+			RightObject.transform.Translate(-speed, 0, 0);
+			// delete note from display
+			if(LeftObject.transform.position.x >= 0) {
+				isMoving = false;
+				this.GetComponent<AudioSource>().Play();
+			}
+		}
+		else {
+			if(!this.GetComponent<AudioSource>().isPlaying) {
+				NoteData.Kill();
+				Object.Destroy(this.gameObject);
+				transform.parent.GetComponent<BeatGenerator>()
+					.NoteList.Dequeue();
+			}
 		}
 	}
 }
