54cf071dcbea8115d859a3b2a8fd95ee2ac50417
Kais_DkM <kais0903@gmail.com>
2015-08-12 18:40:44 +0900

    added InputManager class

---
+++ Assets/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+	public enum InputResult {NONE, DOWN, KEEP, UP}
+
+	public InputResult[] Result;
+	private bool[] isLongPressed;
+
+	// Use this for initialization
+	void Start () {
+		PreviousPressed = new bool[3] {false, false, false};
+		Result = new InputResult[3] {InputManager.InputResult.NONE,
+									 InputManager.InputResult.NONE,
+									 InputManager.InputResult.NONE};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Ready() {
+		
+	}
+}
