75600247ecf1ca0737c8b50c3fc18ac93b0f87fe
lasagnaphil <lasagnaphil@snu.ac.kr>
2015-08-13 00:51:08 +0900

    Load from txt file
    Txt 파일을 로드할 수 있다. 더이상 json 파일로 변환하지 않아도 된다.
    
    txt 파일 편집하는 법:
    1번째 라인: 곡 이름
    2번째 라인: bpm
    3번째 라인: 한 마디당 들어있는 박자 수 (보통 16임)
    4번째 라인부터:
    '0' : 노트
    '-' : 노트X
    ' ' : 빈칸 (아무 기능 없음, 그냥 미관상으로 씀)
    ';' : 플레이어 공격/방어 바꿈 (그 다음 노트부터 바꿔지게 됨)
    엔터 (새 라인)은 아무 상관이 없음. 다만 네마디를 한 줄에 쓰느 것을 원칙으로 함.

--- Assets/BeatGenerator.cs
+++ Assets/BeatGenerator.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 using System.Collections.Generic;
 
 using SimpleJSON;
@@ -13,6 +14,8 @@ public class BeatGenerator : MonoBehaviour {
 
 	public Queue<Note> NoteList = new Queue<Note>(); // List of Notes
 	private int StartTime; // Starting time of battle
+
+	public int bpm; // bpm of song
 
 	// interpret input & put data into queue
 	public void OnSocketRead(string input) {
@@ -31,26 +34,64 @@ public class BeatGenerator : MonoBehaviour {
 	}
 
 	void Start() {
-		// load json file into string
-		string jsonString = Resources.Load<TextAsset>("test_data").text;
+		LoadFromTxt ("test_data");
+		StartTime = (int)System.Math.Round(Time.timeSinceLevelLoad * 1000000);
+		GameObject.Find("NetworkManager").GetComponent<NetworkConnector>()
+			.OnRead = this.OnSocketRead;
+	}
+
+	void LoadDefaultBeat() {
+		for(int i = 0; i <= 100; ++i) {
+			Note newNote = new Note((uint)i, 1500000 * (i + 3));
+			if(i % 7 == 0) newNote.Flip = true;
+			NoteList.Enqueue(newNote);
+		}
+	}
+	// load from json file
+	void LoadFromJSON(string txtpath) {
+		string jsonString = Resources.Load<TextAsset>(txtpath).text;
 		var data = JSON.Parse (jsonString);
 		int noteCount = data["notecount"].AsInt;
 		Debug.Log (noteCount);
 		for (int i = 0; i < noteCount; i++) {
 			Note newNote =
 				new Note((uint)i,
-						 data["notes"][i]["time"].AsInt + BEAT_DELAY);
+				         data["notes"][i]["time"].AsInt + BEAT_DELAY);
 			newNote.Flip = data["notes"][i]["flip"].AsBool;
 			NoteList.Enqueue(newNote);
 		}
-		/*for(int i = 0; i <= 100; ++i) {
-			Note newNote = new Note((uint)i, 1500000 * (i + 3));
-			if(i % 7 == 0) newNote.Flip = true;
-			NoteList.Enqueue(newNote);
-		}*/
-		StartTime = (int)System.Math.Round(Time.timeSinceLevelLoad * 1000000);
-		GameObject.Find("NetworkManager").GetComponent<NetworkConnector>()
-			.OnRead = this.OnSocketRead;
+	}
+	// load from txt file
+	void LoadFromTxt(string txtpath) {
+		string noteString = Resources.Load<TextAsset>(txtpath).text;
+		string[] lines = noteString.Split('\n');
+
+		int bpm = Int32.Parse (lines[1]);
+		int quantize = Int32.Parse (lines[2]);
+		float beatLen = 60.0f / ((float)bpm) * (4.0f/((float)quantize));
+
+		uint count = 0;
+		uint notecount = 0;
+		for(int i = 3; i < lines.Length; i++) {
+			foreach (char c in lines[i]) {
+				switch(c)
+				{
+				case '0':
+					notecount++;
+					Note newNote = new Note(notecount, Convert.ToInt32 (count * beatLen * 1000000) + BEAT_DELAY);
+					newNote.Flip = false;
+					NoteList.Enqueue(newNote);
+					count++;
+					break;
+				case '-':
+					count++; break;
+				case ' ':
+					break;
+				case ';':
+					NoteList.Last ().Flip = true; break;
+				}
+			}
+		}
 	}
 
 	void Update() {
