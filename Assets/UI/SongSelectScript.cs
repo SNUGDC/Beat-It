09b3ad0128b64ee7733eac6386f6f39afd8469e3
Phillip Chang <lasagnaphil@snu.ac.kr>
2015-08-24 22:05:54 +0900

    Added new font

--- Assets/UI/SongSelectScript.cs
+++ Assets/UI/SongSelectScript.cs
@@ -8,7 +8,7 @@ public class SongSelectScript : MonoBehaviour {
 	public PlayerScript player1;
 	public PlayerScript player2;
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Return) && player1.isReady && player2.isReady)
+		if (player1.isReady && player2.isReady)
 			Application.LoadLevel ("sceneBattle");
 	}
 }
